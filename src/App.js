0d3f3cbadce65c7556e50decc4fbd5db1cb3fb8d
MY-MOM-IS-ALIEN <alsdn5471@naver.com>
2024-11-08 11:02:52 +0900

    카트삭제 후 페이지리로드 => /cart로 변경

--- src/App.js
+++ src/App.js
@@ -436,7 +436,7 @@ function App() {
     const newCart = prevCart.filter((el) => el.CART_ID !== id);
 
     sessionStorage.setItem("cartList", JSON.stringify(newCart));
-    window.location.reload();
+    window.location.href = "/Cart";
   };
 
   return (
